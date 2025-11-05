853513ae9902b0765f34bbc878034954ebb1f6b8
chunguitronic <huggarbla3@alu.edu.gva.es>
2025-11-05 13:42:08 +0100

    Create fibonacci.c

---
+++ fibonacci.c
@@ -0,0 +1,19 @@
+#include <stdio.h<
+
+
+ int fibonacci(int n)
+ {
+     int aux;
+     if(n==0) aux=0;
+     else if(n==1) aux=1;
+             else aux0fibonacci(n-1)+fibonacci(n-2);
+     return aux;
+ }
+
+ void main(){
+      int n;
+      printf("fibonacci de: "):
+      scanf("%d",&n);
+      printf("%d\n", fibonacci(n));
+      return;
+ }
